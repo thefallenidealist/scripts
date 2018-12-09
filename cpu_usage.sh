93763a706b12772ce154630c48cc2a2fed50139e
Johnny Sorocil <propaliidealist@gmail.com>
2018-12-09 20:39:44 +0100

    cpu-usage updated

--- cpu_usage.sh
+++ cpu_usage.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 # Created 181110
+# INFO 181209: Show one percetange for all CPUs [0 - 100%]
 
-# Print Usage of CPU0 and CPU1
-# convert float to integer (at that the reason of redirecting stderr)
-printf "CPU0: %3d%% CPU1: %3d%%\n" $(sysctl -n dev.cpu.0.cx_usage | cut -d '%' -f 1) $(sysctl -n dev.cpu.1.cx_usage | cut -d '%' -f 1) 2>/dev/null
-printf "CPU2: %3d%% CPU3: %3d%%\n" $(sysctl -n dev.cpu.2.cx_usage | cut -d '%' -f 1) $(sysctl -n dev.cpu.3.cx_usage | cut -d '%' -f 1) 2>/dev/null
-# TODO 181110: one sysctl instead of 2/4
+NCPU=$(sysctl -n hw.ncpu)
+# expr needs spaces around "*" signal
+PERCENTAGE=$(expr $NCPU \* 100)
+
+# process 11 is "[idle]" kernel thread
+# ps u 11 | awk 'NR==2 {print 400 - $3}'
+IDLE=$(ps u 11 | awk 'NR==2 {print $3}')
+USAGE=$(bc -e "scale=2; ($NCPU * 100 - $IDLE) / $NCPU " -e quit)
+
+printf "%.1f %%\n" $USAGE
