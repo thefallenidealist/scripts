f92d2bab5d84c599f4b870cf242013204962ac62
Johnny Sorocil <propaliidealist@gmail.com>
2019-12-16 21:55:41 +0100

    pare naracunaj update

--- pare-naracunaj.sh
+++ pare-naracunaj.sh
@@ -9,19 +9,22 @@ DIR="$HOME/docs-privat"
 FILE="$DIR/pare$YEAR$MONTH"
 FILE_KEYWORDS="$DIR/pare-keywords"
 
-if [ "$#" -gt 1 ]; then
+if [ "$#" -gt 0 ]; then
 	FILE=$1
 fi
 
 echo File: $FILE
 echo "----------------------------------------"
-while read LINE
+while read LINE		# iterate through every file of file
 do
-	TMP=$(grep -E -i $LINE $FILE | cut -f 1 | sed -e 's/#.*$//' -e '/^$/d' | cut -d " " -f 1 | paste -sd+ - | bc)
+	# grep for word, -E (extended regex for item1a|item1b|item1c, remove comments, remove empry line, print only 1st field, paste numbers and '+' and run it through calculator
+	TMP=$(grep -Ew $LINE $FILE | cut -f 1 | sed -e 's/#.*$//' -e '/^$/d' | cut -d " " -f 1 | paste -sd+ - | bc)
+
 	case $TMP in
 		(*[!0-9]*|'')	# if not a number
 			;;
 		(*)
+			# echo Potroseno: $POTROSENO tmp: $TMP line: $LINE
 			POTROSENO=$(($POTROSENO+$TMP))
 			;;
 	esac
@@ -29,18 +32,35 @@ do
 	case "$LINE" in
 		*\|*)
 			# If line contains '|' show only first member:
-			echo $(echo $LINE | cut -d '|' -f 1) $TMP
+			echo $(echo $LINE | cut -d '|' -f 1): $TMP
 			;;
 		*)
 			echo $LINE: $TMP
 			;;
 	esac
+
+	if [ "$LINE" = "stednja" ] ; then
+		STEDNJA=$TMP
+	fi
+
+	# rezije|stan|stanarina
+	case "$LINE" in
+		*rezije*)
+			REZIJE=$TMP
+			;;
+	esac
+
 done < $FILE_KEYWORDS
 
 UKUPNO=`cat $FILE | cut -f 1 | sed -e 's/#.*$//' -e '/^$/d' | cut -d " " -f 1 | paste -sd+ - | bc`
 NESVRSTANO=$(($UKUPNO-$POTROSENO))
+UKUPNO_BEZ_STEDNJE_I_REZIJA=$(($UKUPNO-$STEDNJA-$REZIJE))
 
 echo "----------------------------------------"
-echo "Potroseno:  $POTROSENO"
-echo "Nesvrstano: $NESVRSTANO"
-echo "Ukupno:     $UKUPNO"
+echo "Ukupno:      $UKUPNO"
+# echo "Potroseno:   $POTROSENO"
+echo "Rezije:      $REZIJE"
+echo "Stednja:     $STEDNJA"
+echo "Nesvrstano:  $NESVRSTANO"
+echo "----------------------------------------"
+echo "Potroseno:   $UKUPNO_BEZ_STEDNJE_I_REZIJA   (bez stednje i rezija)"
